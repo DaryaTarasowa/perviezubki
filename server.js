3e2c1da239c10b29657d659c6402307d7d19a88a
vassilisha <darya.tarasowa@gmail.com>
2020-01-13 22:19:57 +0100

    login page

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '185.224.138.154',
+  user     : 'u543051139_vassilisha',
+  password : 'rfhfvtkm123',
+  database : 'u543051139_teeth'
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// connection.connect(function(err){
+// if(!err) {
+//     console.log("Database is connected ... \n\n");
+// } else {
+//     console.log("Error connecting database ... \n\n");
+// }});
 
 app.use(express.static(path.join(__dirname, 'build'), {
   index: false,
@@ -10,13 +26,6 @@ app.use(express.static(path.join(__dirname, 'build'), {
 }));
 
 
-
-// app.use(express.static(__dirname + "/public", {
-//     index: false,
-//     immutable: true,
-//     cacheControl: true,
-//     maxAge: "30d"
-// }));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import LoginBox from "./LoginBox";
+import Register from "./Register";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends Component {
+  state = {
+    LoginOnTop: true
+  };
+
+  clicked = () => {
+    this.setState(prevState => ({ LoginOnTop: !prevState.LoginOnTop }));
+  };
+  render() {
+    return (
+      <div className="App">
+        <LoginBox clickedState={this.state.LoginOnTop} clicked={this.clicked} />
+        <Register clickedState={this.state.LoginOnTop} clicked={this.clicked} />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
